291bfb5d7afc2a71fc70189e71e05a0e4488f1e4
Idanos99 <100779391+Idanos99@users.noreply.github.com>
2025-01-20 17:19:13 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,31 +1,30 @@
 import socket
 import threading
+import datetime
+
+PORT = 1234
 
 # Function to receive messages from the server
 def receive_messages(client_socket):
     while True:
-        try:
-            message = client_socket.recv(1024).decode('utf-8')
-            if message:
-                print(message)
-        except:
-            break
+        message = client_socket.recv(1024).decode('utf-8')
+        if message:
+            t = datetime.datetime.now()
+            t = t.strftime('%H:%M:%S')
+            print(t + " - " + message)
 
 # Function to send messages to the server
 def send_message(client_socket):
     while True:
         message = input()
-        if message == "/exit": # Quit from the chat
-            client_socket.close()
-            exit(0)
-        print ('\033[1A' + '\033[K', end ='') # Delete my message
+        print ('\033[1A' + '\033[K', end ='')
         if message:
             client_socket.send(message.encode('utf-8'))
 
 # Function to start the client
 def start_client():
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', 12345))  # Connect to the server at localhost:12345
+    client_socket.connect(('localhost', PORT))  # Connect to the server at localhost:12345
 
     # Send the client's name to the server
     client_name = input("Enter your name: ")
